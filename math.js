09632ef521ca0dd9461a4dbabfda6bcc2e197c5b
Nerfe5 <n3rfe5@gmail.com>
2023-06-22 08:24:37 -0600

    Correción de ortografía y continuación de fórmulas para los polígonos más comúnes

--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+
+console.group('Cuadrado')
 const ladoCuadrado = 5;
 const perimetroCuadro = ladoCuadrado * 4;
 
@@ -17,6 +19,9 @@ console.log({
     areaCuadrado
 });
 
+console.groupEnd('Cuadrado');
+
+console.group('Triangulo')
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const ladoTrianguloBase = 4;
@@ -34,6 +39,24 @@ function calcularTriangulo(lado1, lado2, base, altura){
  }   
 }
 
+
+function calcularAlturaTriangulo(ladoA, base){
+    if(ladoA == base){
+        console.warn('Este no es un triangulo isosceles');
+    }
+    else{
+        //h = raizcuadrada(ladoA**2) - (b**2)/4)
+        return Math.sqrt((ladoA **2) - (base**2) /4 );
+    }
+}
+
+function calcularAlturaEscaleno(lado1, lado2, lado3){
+    if (lado1 == lado2){
+        console.warn('false')
+    } else{
+        
+    }
+}
 console.log({
     ladoTriangulo1,
     ladoTriangulo2,
@@ -42,3 +65,31 @@ console.log({
     perimetroTriangulo,
     areaTriangulo 
 })
+
+
+console.groupEnd('Triangulo');
+
+console.group('Circulo')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1416;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+console.log({
+radioCirculo,
+diametroCirculo,
+circunferencia,
+areaCirculo,
+})
+
+function calcularCirculo(radio){
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+    return{
+        circunferencia:diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI, 
+    }
+}
+console.groupEnd('Circulo');
